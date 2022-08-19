151f7709a6bae2d0f42e22c006beef478067c642
derekbeistad <derek18b@gmail.com>
2022-08-19 16:19:34 -0400

    Website Update

--- main.py
+++ main.py
@@ -7,19 +7,9 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/code-portfolio")
-def code():
-    return render_template("python.html")
-
-
-@app.route("/photo-portfolio")
-def photo():
-    return render_template("photo.html")
-
-
-@app.route("/video-portfolio")
-def video():
-    return render_template("video.html")
+@app.route("/dev")
+def dev():
+    return render_template("developer.html")
 
 
 if __name__ == "__main__":
